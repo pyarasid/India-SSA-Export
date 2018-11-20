ca40454d116a387a41dab852db11787c42774853
unknown <siddharth.dixit47@gmail.com>
2018-11-19 23:20:38 -0500

    Updates (19th Nov)

---
+++ Export.R
@@ -0,0 +1,26 @@
+#load relevant libraries
+library(tidyverse)
+library(reshape2)
+library(readxl)
+library(ggplot2)
+library(dplyr)
+
+#load excel dataset
+india_export <- read_excel("Export-Import.xlsx", sheet= "Export")
+View(india_export)
+
+#rename columns
+india_export <- india_export %>% rename(country=`Reporter Name`) %>% 
+              rename(partner= `Partner Name`) %>% 
+              rename(group= `Product Group`) %>% 
+              rename(Export= `Trade Flow`)
+
+#converting to tidy data
+india_export <- melt(india_export, id.vars =c("country", "Export", "group", "Indicator"),
+                     measure.vars = c("1989", "1990", "1991", "1992", "1993", "1994", "1995", "1996", "1997", "1998", "1999", "2000", "2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016"),
+                     variable.name = "Year", value.name = "trade_value")
+
+#creating plot of export value
+p1 <- ggplot(data = india_export %>% filter(Indicator=="Export (US$ Million)"), mapping = aes(x=Year, y= trade_value)) 
+     p1 + geom_line(aes(group=group, color= group))
+
