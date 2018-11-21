fb3ccbd9b6cea91c7bb774bacfb080bf0d0f4cf7
unknown <siddharth.dixit47@gmail.com>
2018-11-20 23:35:27 -0500

    Updates(20th Nov)

--- Export.R
+++ Export.R
@@ -4,6 +4,7 @@ library(reshape2)
 library(readxl)
 library(ggplot2)
 library(dplyr)
+library(scales)
 
 #load excel dataset
 india_export <- read_excel("Export-Import.xlsx", sheet= "Export")
@@ -22,5 +23,17 @@ india_export <- melt(india_export, id.vars =c("country", "Export", "group", "Ind
 
 #creating plot of export value
 p1 <- ggplot(data = india_export %>% filter(Indicator=="Export (US$ Million)"), mapping = aes(x=Year, y= trade_value)) 
-     p1 + geom_line(aes(group=group, color= group))
-
+     p1 + geom_line(aes(group=group, color= group), size=1) +geom_point(aes(color=group))+ 
+          scale_y_continuous(label=comma) +
+         theme(axis.text.x = element_text(angle = 55, hjust=1), axis.text.y = element_text(angle = 55)) +
+        labs(x="Years", y="Trade Value (in Million Dollars)", color="Product Group")
+     
+#creating plots of export share
+p2 <- ggplot(data = india_export %>% filter(Indicator=="Export-Share"), mapping = aes(x=Year, y=trade_value))
+      p2 + geom_line(aes(group=group, colour=group), size=1)+geom_point(aes(color=group))+
+        scale_y_continuous(labels = scales::percent_format(accuracy = 1)) +
+        theme(axis.text.x = element_text(angle = 55, hjust=1), axis.text.y = element_text(angle=55))+
+        labs(x="Years", y="Export Share (in percentage)", color="Product Group")
+        
+     
+     
